32c16a482c8edc774aa9282673d4aa7acaf279d9
tim-stone <tim@twoimpress.com>
2021-02-25 22:51:23 -0600

    1.07
    Slider function completed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
-  $('.main-slider').slick({
-    
+
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '5px',
+    slidesToShow: 3,
+    arrows: true,
+    prevArrow: '.slick-prev',
+    nextArrow: '.slick-next',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3,
+          touchMove: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          touchMove: true
+        }
+      }
+    ]
   });
+
 });
